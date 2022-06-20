d0d4a56f7f12f73ff3bc2471d0e554561e17d3e5
Jesse Sheehan <jesse@sheehan.nz>
2022-06-21 06:35:59 +1200

    Remove performance counter modules

--- lib/performance_counter.mli
+++
@@ -1,8 +0,0 @@
-type 'a performance_counter
-
-val create : int -> 'a performance_counter
-val push : 'a performance_counter -> 'a -> 'a performance_counter
-val clear : 'a performance_counter -> 'a performance_counter
-val compute : 'a performance_counter -> ('a list -> 'b) -> 'b
-val length : 'a performance_counter -> int
-val nth : 'a performance_counter -> int -> 'a
