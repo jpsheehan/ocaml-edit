8dbb1c0760635f69837fb9741b9734bb9eaea659
Jesse Sheehan <jesse@sheehan.nz>
2022-06-29 07:00:58 +1200

    Fixed the selection color bug

--- lib/theme.ml
+++ lib/theme.ml
@@ -6,10 +6,15 @@ type t = {
   text_font : SdlContext.font;
 }
 
-let create (font_location, font_size) fg_color bg_color cursor_color
-    selection_color =
+let create (font_location, font_size) ~fg ~bg ~cursor ~selection =
   let text_font = SdlContext.font_create font_location font_size in
-  { text_font; fg_color; bg_color; cursor_color; selection_color }
+  {
+    text_font;
+    fg_color = fg;
+    bg_color = bg;
+    cursor_color = cursor;
+    selection_color = selection;
+  }
 
 let get_fg_color t = t.fg_color
 let get_bg_color t = t.bg_color
--- lib/theme.mli
+++ lib/theme.mli
@@ -1,6 +1,13 @@
 type t
 
-val create : string * int -> Color.t -> Color.t -> Color.t -> Color.t -> t
+val create :
+  string * int ->
+  fg:Color.t ->
+  bg:Color.t ->
+  cursor:Color.t ->
+  selection:Color.t ->
+  t
+
 val get_fg_color : t -> Color.t
 val get_bg_color : t -> Color.t
 val get_selection_color : t -> Color.t
